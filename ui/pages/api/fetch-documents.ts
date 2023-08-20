3710e5b86b5962ea4a773d59ae22403feddb95ce
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-08-20 02:22:29 +0200

    Update fetch-documents.ts

--- ui/pages/api/fetch-documents.ts
+++ ui/pages/api/fetch-documents.ts
@@ -2,24 +2,25 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { ChromaClient, TransformersEmbeddingFunction } from "chromadb";
 
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
     const client = new ChromaClient({
-      path: "http://localhost:8000"
+      path: "http://localhost:8000",
     });
 
-  const query = req.body.query
+    const query = req.body.query;
+    const embedder = new TransformersEmbeddingFunction();
 
-  const embedder = new TransformersEmbeddingFunction();
+    const collection = await client.getCollection({ name: "hypzert-dokumentation", embeddingFunction: embedder });
 
-  const collection = await client.getCollection({name: "hypzert-dokumentation", embeddingFunction: embedder})
+    const results = await collection.query({
+      nResults: 2,
+      queryTexts: [query],
+    });
 
-  const results = await collection.query({
-    nResults: 2, 
-    queryTexts: ["Hi"],
-  }) 
-
-  res.status(200).json(results);
+    res.status(200).json(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An unexpected error occurred' });
+  }
 }
