79af0786b229592a3874ba6348335abd758bd1e3
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-08-25 22:30:46 +0200

    Update Import.tsx

--- ui/components/Settings/Import.tsx
+++ ui/components/Settings/Import.tsx
@@ -1,14 +1,14 @@
 import { IconFileImport } from '@tabler/icons-react';
 import { FC } from 'react';
+import { toast } from 'react-hot-toast';
 
 import { useTranslation } from 'next-i18next';
 
 import { SupportedExportFormats } from '@/types/export';
 
 import { SidebarButton } from '../Sidebar/SidebarButton';
-import axios from 'axios';
 
-import { toast } from 'react-hot-toast';
+import axios from 'axios';
 
 interface Props {
   onImport: (data: SupportedExportFormats) => void;
@@ -27,32 +27,33 @@ export const Import: FC<Props> = ({ onImport }) => {
         accept=".pdf"
         onChange={async (e) => {
           if (!e.target.files?.length) return;
-        
-          const file = e.target.files[0];
-    const formData = new FormData();
-    formData.append('pdf', file);
 
-    axios.post('/api/inject-documents', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
-    .then(response => {
-      toast.success('File uploaded successfully!');
-    })
-    .catch(error => {
-      console.error('Error uploading file:', error);
-    });
-  
+          const file = e.target.files[0];
+          const formData = new FormData();
+          formData.append('pdf', file);
+
+          axios
+            .post('/api/inject-documents', formData, {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+              },
+            })
+            .then((response) => {
+              toast.success('File uploaded successfully!');
+            })
+            .catch((error) => {
+              console.error('Error uploading file:', error);
+            });
         }}
-        
       />
 
       <SidebarButton
         text={t('Upload file')}
         icon={<IconFileImport size={18} />}
         onClick={() => {
-          const importFile = document.querySelector('#import-file') as HTMLInputElement;
+          const importFile = document.querySelector(
+            '#import-file',
+          ) as HTMLInputElement;
           if (importFile) {
             importFile.click();
           }
@@ -61,4 +62,3 @@ export const Import: FC<Props> = ({ onImport }) => {
     </>
   );
 };
-
