5937b83c14fb15ee6cb8d22ee7943046a1f7b7e1
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-08-25 21:08:44 +0200

    Update chat.ts

--- ui/pages/api/chat.ts
+++ ui/pages/api/chat.ts
@@ -1,8 +1,5 @@
 import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
-import { loadQAMapReduceChain } from "langchain/chains";
-import { Document } from "langchain/document";
-
 
 import { ChatBody, Message } from '@/types/chat';
 
@@ -16,37 +13,9 @@ export const config = {
   runtime: 'edge',
 };
 
-// Function to fetch and format documents
-async function fetchAndFormatDocuments(lastMessageContent: string) {
-  try {
-    const response = await fetch('http://localhost:3000/api/fetch-documents', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ input: lastMessageContent }),
-    });
-    
-    if (!response.ok) {
-      throw new Error(`Error fetching documents: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    const result = data.metadatas[0].map((metadata: any, index: number) => {
-      return `Source ${index + 1}) ${metadata.title}, ${metadata.page}: ${data.documents[0][index]}\n`;
-    }).join('');
-
-    return result;
-
-  } catch (error) {
-    console.error('Error fetching and formatting documents:', error);
-    throw error; // You may want to throw a more specific error object here
-  }
-}
-
-
 const handler = async (req: Request): Promise<Response> => {
   try {
-    const { model, messages, key, prompt, temperature } =
-      (await req.json()) as ChatBody;
+    const { model, messages, key, prompt, temperature } = (await req.json()) as ChatBody;
 
     await init((imports) => WebAssembly.instantiate(wasm, imports));
     const encoding = new Tiktoken(
@@ -60,10 +29,6 @@ const handler = async (req: Request): Promise<Response> => {
       promptToSend = DEFAULT_SYSTEM_PROMPT;
     }
 
-    const lastMessage = messages[messages.length - 1];
-
-    const relevantDocuments = await fetchAndFormatDocuments(lastMessage.content);
-    
     let temperatureToUse = temperature;
     if (temperatureToUse == null) {
       temperatureToUse = DEFAULT_TEMPERATURE;
@@ -74,42 +39,22 @@ const handler = async (req: Request): Promise<Response> => {
     let tokenCount = prompt_tokens.length;
     let messagesToSend: Message[] = [];
 
-    // Constructing the last message with the relevant documents
-    const lastMessageContent = `Respond to the following message: ${lastMessage.content} using the following context: ${relevantDocuments}`;
-    const lastMessageTokens = encoding.encode(lastMessageContent);
-    tokenCount += lastMessageTokens.length;
-
-    // Adding existing messages
     for (let i = messages.length - 1; i >= 0; i--) {
       const message = messages[i];
       const tokens = encoding.encode(message.content);
 
       if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
-        continue; // Skip this message if adding it would exceed the token limit
+        break;
       }
       tokenCount += tokens.length;
       messagesToSend = [message, ...messagesToSend];
     }
 
-    // Add the constructed last message
-    messagesToSend = [
-      ...messagesToSend,
-      { role: 'user', content: lastMessageContent },
-    ];
-
     encoding.free();
 
-    console.log(model, promptToSend, temperatureToUse, key, messagesToSend);
+    console.log(model,  promptToSend, temperatureToUse, key, messagesToSend);
 
-    
-
-    const stream = await OpenAIStream(
-      model,
-      promptToSend,
-      temperatureToUse,
-      key,
-      messagesToSend,
-    );
+    const stream = await OpenAIStream(model, promptToSend, temperatureToUse, key, messagesToSend);
 
     return new Response(stream);
   } catch (error) {
