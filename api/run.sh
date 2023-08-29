4c7d02815760413dfbb86fa724e6e30bb650be12
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-08-29 02:42:58 +0200

    Update run.sh

--- api/run.sh
+++ api/run.sh
@@ -1,42 +1,35 @@
 #!/bin/bash
 
- # Check if the MODEL environment variable is set
- if [ -z "$MODEL" ]
- then
-     echo "Please set the MODEL_FILE environment variable"
-     exit 1
- fi
+if [ -z "$MODEL" ]
+then
+    echo "Please set the MODEL_FILE environment variable"
+    exit 1
+fi
 
- # Check if the MODEL_DOWNLOAD_URL environment variable is set
- if [ -z "$MODEL_DOWNLOAD_URL" ]
- then
-     echo "Please set the MODEL_DOWNLOAD_URL environment variable"
-     exit 1
- fi
+if [ -z "$MODEL_DOWNLOAD_URL" ]
+then
+    echo "Please set the MODEL_DOWNLOAD_URL environment variable"
+    exit 1
+fi
 
- # Check if the model file exists
- if [ ! -f $MODEL ]; then
-     echo "Model file not found. Downloading..."
-     # Check if curl is installed
-     if ! [ -x "$(command -v curl)" ]; then
-         echo "curl is not installed. Installing..."
-         apt-get update --yes --quiet
-         apt-get install --yes --quiet curl
-     fi
-     # Download the model file
-     curl -L -o $MODEL $MODEL_DOWNLOAD_URL
-     if [ $? -ne 0 ]; then
-         echo "Download failed. Trying with TLS 1.2..."
-         curl -L --tlsv1.2 -o $MODEL $MODEL_DOWNLOAD_URL
-     fi
- else
-     echo "$MODEL model found."
- fi
+# check if curl is installed
 
-# Build the project
+if ! [ -x "$(command -v curl)" ]; then
+    echo "curl is not installed. Installing..."
+    apt-get update --yes --quiet
+    apt-get install --yes --quiet curl
+fi
+
+
+if [ ! -f $MODEL ]; then
+    echo "Model file not found. Downloading..."
+    curl -L -o $MODEL $MODEL_DOWNLOAD_URL
+else
+    echo "$MODEL model found."
+fi
+
 make build
-
-# Get the number of available CPU threads
+# Get the number of available threads on the system
 n_threads=$(grep -c ^processor /proc/cpuinfo)
 
 # Define context window
@@ -45,19 +38,18 @@ n_ctx=4096
 # Offload everything to CPU
 n_gpu_layers=0
 
-# Define batch size based on total RAM
-total_ram=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+# Define batch size
 n_batch=2096
+# If total RAM is less than 8GB, set batch size to 1024
+total_ram=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
 if [ $total_ram -lt 8000000 ]; then
     n_batch=1024
 fi
 
-# Display configuration information
 echo "Initializing server with:"
 echo "Batch size: $n_batch"
 echo "Number of CPU threads: $n_threads"
 echo "Number of GPU layers: $n_gpu_layers"
 echo "Context window: $n_ctx"
 
-# Run the server
 exec python3 -m llama_cpp.server --n_ctx $n_ctx --n_threads $n_threads --n_gpu_layers $n_gpu_layers --n_batch $n_batch
