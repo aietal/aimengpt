9dac5b5c553ddbee253a029b2f729e37557a9072
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-08-30 18:35:07 +0200

    Update rag-chat.ts

--- ui/pages/api/rag-chat.ts
+++ ui/pages/api/rag-chat.ts
@@ -2,7 +2,7 @@ import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
 import { loadQAMapReduceChain } from "langchain/chains";
 import { Document } from "langchain/document";
-
+import { codeBlock, oneLine } from 'common-tags'
 
 import { ChatBody, Message } from '@/types/chat';
 
@@ -32,7 +32,7 @@ async function fetchAndFormatDocuments(lastMessageContent: string) {
 
     const data = await response.json();
     const result = data.metadatas[0].map((metadata: any, index: number) => {
-      return `Source ${index + 1}) ${metadata.title}, ${metadata.page}: ${data.documents[0][index]}\n`;
+      return `Source ${index + 1}) Title: ${metadata.title}, Page: ${metadata.page}, Content: ${data.documents[0][index]}\n`;
     }).join('');
 
     console.log(result);
@@ -46,8 +46,11 @@ async function fetchAndFormatDocuments(lastMessageContent: string) {
 }
 
 
+
+
+
 const handler = async (req: Request): Promise<Response> => {
-  console.log("RAG Baby")
+
   try {
     const { model, messages, key, prompt, temperature } =
       (await req.json()) as ChatBody;
@@ -59,7 +62,25 @@ const handler = async (req: Request): Promise<Response> => {
       tiktokenModel.pat_str,
     );
 
-    let promptToSend = prompt;
+    let promptToSend = codeBlock`
+    ${oneLine`
+      You are a very enthusiastic AI assistant  who loves
+      to help people! Given the following information from
+      relevant documentation, answer the user's question using
+      only that information, outputted in markdown format.
+    `}
+
+    ${oneLine`
+      If you are unsure
+      and the answer is not explicitly written in the documentation, say
+      "Sorry, I don't know how to help with that."
+    `}
+    
+    ${oneLine`
+      Always include citations from the documentation.
+    `}
+  `;
+
     if (!promptToSend) {
       promptToSend = DEFAULT_SYSTEM_PROMPT;
     }
@@ -79,38 +100,98 @@ const handler = async (req: Request): Promise<Response> => {
     let messagesToSend: Message[] = [];
 
     // Constructing the last message with the relevant documents
-    const lastMessageContent = `Respond to the following message: ${lastMessage.content} using the following context: ${relevantDocuments}`;
-    const lastMessageTokens = encoding.encode(lastMessageContent);
-    tokenCount += lastMessageTokens.length;
+    // const lastMessageContent = `Respond to the following message: ${lastMessage.content} using the following context: ${relevantDocuments}`;
+    // const lastMessageTokens = encoding.encode(lastMessageContent);
+    // tokenCount += lastMessageTokens.length;
 
     // Adding existing messages
-    for (let i = messages.length - 1; i >= 0; i--) {
-      const message = messages[i];
-      const tokens = encoding.encode(message.content);
+    // for (let i = messages.length - 1; i >= 0; i--) {
+    //   const message = messages[i];
+    //   const tokens = encoding.encode(message.content);
 
-      if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
-        continue; // Skip this message if adding it would exceed the token limit
-      }
-      tokenCount += tokens.length;
-      messagesToSend = [message, ...messagesToSend];
-    }
+    //   if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
+    //     continue; // Skip this message if adding it would exceed the token limit
+    //   }
+    //   tokenCount += tokens.length;
+    //   messagesToSend = [message, ...messagesToSend];
+    // }
 
     // Add the constructed last message
-    messagesToSend = [
-      ...messagesToSend,
-      { role: 'user', content: lastMessageContent },
-    ];
+  
 
     encoding.free();
 
-    console.log("rag Bruda",model, promptToSend, temperatureToUse, key, messagesToSend);
+    console.log(model, promptToSend, temperatureToUse, key, messagesToSend);
 
-    
+    // const messageToSend = codeBlock`
+    //   ${oneLine`
+    //     You are a very enthusiastic AI assistant who loves
+    //     to help people! Given the following sections from relevant
+    //     documentation, answer the question using only that information,
+    //     outputted in markdown format. You must in the same language as the question. If you are unsure and the answer
+    //     is not explicitly written in the documentation, say
+    //     "Sorry, I don't know how to help with that."
+    //   `}
+
+    //   Context sections:
+    //   ${relevantDocuments}
+
+    //   Question: """
+    //   ${lastMessage.content}
+    //   """
+
+    //   Answer as markdown (including related code snippets if available):
+    // `
+
+    // messagesToSend = [
+    //   { role: 'user', content: messageToSend },
+    // ];
+
+  messagesToSend = [
+      {
+        role: "user",
+        content: codeBlock`
+          Here is the relevant documentation:
+          ${relevantDocuments}
+        `,
+      },
+      {
+        role: "user",
+        content: codeBlock`
+          ${oneLine`
+            Answer my next question using only the above documentation.
+            You must also follow the below rules when answering:
+          `}
+          ${oneLine`
+            - Do not make up answers that are not provided in the documentation.
+          `}
+          ${oneLine`
+            - If you are unsure and the answer is not explicitly written
+            in the documentation context, say
+            "Sorry, I don't know how to help with that."
+          `}
+          ${oneLine`
+            - Prefer splitting your response into multiple paragraphs.
+          `}
+          ${oneLine`
+            - Output as markdown with citations based on the documentation.
+          `}
+        `,
+      },
+      {
+        role: "user",
+        content: codeBlock`
+          Here is my question:
+          ${oneLine`${lastMessage.content}`}
+      `,
+      },
+    ]
+
 
     const stream = await OpenAIStream(
       model,
       promptToSend,
-      temperatureToUse,
+      0,
       key,
       messagesToSend,
     );
